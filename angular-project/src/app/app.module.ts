6fa1ad24d7c18d298815cc6f06a95de236b7e95f
Sasha191261 <sash.klub@gmail.com>
2020-02-26 01:37:58 +0300

    first commit

---
+++ angular-project/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HashLocationStrategy, LocationStrategy } from "@angular/common";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
